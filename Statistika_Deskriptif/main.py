68f1109a9ac7020982e280330d3e10f1220fb1fb
FerdianFy13 <ferdianfy13@gmail.com>
2022-06-28 20:24:41 +0700

    fundamental start statistics

---
+++ Statistika_Deskriptif/main.py
@@ -0,0 +1,26 @@
+# pada era industry 4.0 penggunaan statistika sering digunakan dalam ilmu-ilmu sains dalam perhitungan kalkulus dalam data yang banyak
+# yang akan memudahkan ketika developing sebuah project maupun package yang ingin didistribustikan misalnya
+# data science, machine learning, data mining, data warehouse, and big data analyst
+
+# fundamental statisctis
+# statistika adalah sebuah bidang keilmuan yang mencakup beberapa elemen penting, seperti
+# pengumpulan (collecting), pengelolaan(organising), analisis, dan interpretasi (interpreting)
+#  data dalam pengambilan sebuah keputusan yang akan dihasilkan
+
+# fundamental data
+# data adalah sekumpulan informasi yang diperoleh melalui
+# pengamatan, perhitungan, pengukuran, dan responsi
+
+# fundamental data set
+# dalam data set pada bidang stastika terdapat dua macam, yaitu populasi dan sampel
+# populasi adalah sekumpulan dari keseluruhan data terhadap topik yang akan dikaji
+# sampel adalah bagian dari populasi
+
+# fundamental parameter dan statistic
+# parameter adalah deskripsi numerik dari karakteristik sebuah populasi, dimana dalam populasi hanya terdapat satu paramter untuk satu populasi
+# statistic adalah deskripsi numerik dari karakteristik sebuah sampel yang memungkinkan untuk mendapatkan sejumlah statistik dengan nilai yang beragama dalam satu populasi yang sama
+
+# dalam statistika terdapat dua cabang keilmuan
+# statistika deskriptif adalah sebuah cabang keilmuan yang berfokus pada
+# pengelollan (organistation), peringkasan(summarisation), dan visualisasi data
+# sedangkan statistika inferensi adalah sebuah cabang keilmuan yang berfokus pada pemanfaatna sampel dalam menarik kesimpulan terhadap suatu populasi
