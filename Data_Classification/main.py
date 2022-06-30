6088cd3ccefabd09a31fd53b18bdf9b1c501adeb
FerdianFy13 <ferdianfy13@gmail.com>
2022-06-30 09:32:59 +0700

    data classification in statistics material

---
+++ Data_Classification/main.py
@@ -0,0 +1,44 @@
+# dalam data klasifikasi terdapat dua macam tipe data, yaitu
+# data kualitatif adalah sebuah tipe data non numerik, seperti atribut dan label
+# data kuantitatif adalah sebuah tipe data numeri yang menghasilkan perhitungan dan pengukuran
+
+# example tipe data
+# dalam sebuah kedai makanan terdapat daftar menu makanan dan harga makanan, dimana
+# daftar menu makanan adalah sebuah tipe data kualitatif, sedangkan harga merupakan sebuah tipe data kuantitatif
+
+# skala pengukuran dalam bidang statistika
+# terdapat empat macam skala pengukuran or level of measurements dalam bidang statistika, yaitu
+# nominal, ordinal, interval, rasio,
+
+# skala pengukuran nominal
+# skala pengukuran yang berasosiasi dengan tipe data kualitatif yang dikelompokkan atau dikategorikan berdasarkan, nama, label, and kualitas
+# yang tidak dapat dilakukannya operasi matematika
+# example, merek kendaraaan bermotor, kota kelahiran,
+
+# skala pengukuran ordinal
+# skala pengukuran yang berasosiasi dengan tipe data kualitatif yang dikelompokkan berdasarkan urutan, peringkat, rangking
+# yang tidak dapat dilakukannya operais matematika
+# example, peringkat siswa kelas berdasarkan indeks prestasi komulatif, temperatur udara misalnya panas, sejuk, dingin
+
+# skala pengukuran interval
+# skala pengukuran yang berasosisasi dengan tipe data kuantitatif yaang dikategorikan berdasarkan urutan maupun peringkat
+# yang dapat dikenakan operasi matematika untuk menghitung selisih nilai
+# nilai numerik yang merepresentasikan posisi pada suatu skala ukur tertentu
+# tidak memiliki nilai nol yang absolut atau no inherent zero
+# tidak dapat dikenakan operasi matematika terkaitt perkalian maupun pembagian
+# example, tahun 2020 yang tidak merepresentasikan umur bumi pada tahun 2020, temperature udara 32 derajad celcius
+
+
+# skala pengukuran rasio atau ratio
+# skala pengukuran yang berasosiasi dengan tipe data kuantitatif yang dapat  dikategorikan berdasarkan urutan, peringkat, rangking
+# yangdapat dikenakan operasi matematika untuk menghitung selisih nilai
+# memiliki nilai nol yang absoulut atau inherent zero
+# dapat dikenakan operasi matematika terkait perkalian maupun pembagian
+# example, usia anak tujuh tahun yang merepresentasikan umur dari si anak tersebut, berat badan 79 lg, harga nasi pecel 25k
+
+# tabel skala pengukuran
+#           |pengelompokan data|  |pengurutan data| |selisih data| |perkalian data|
+# nominal   ya
+# ordinal   ya                  ya              tidak
+# interval  ya                  ya              ya
+# ratio     ya                  ya              ya            ya
