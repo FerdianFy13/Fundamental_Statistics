1172c662f4feba2eb1960c1af547f9a815985d16
FerdianFy13 <ferdianfy13@gmail.com>
2022-06-30 19:38:08 +0700

    Design Experimental Material

---
+++ Desaign_Experimen/main.py
@@ -0,0 +1,22 @@
+# dalam bidang statistika studi statistik dibagi menjadi dua macam, yaitu
+# studi observasi adalah studi dengan melalakukan pengamatan langsung terhadap seubjek tanpa melakukan
+# tindakan apapun yang dapat mempengaruhi hasil pengamatan 
+# studi ekspermin adalah studi dengan menerapkan sebuah treatment tertentu terhadap subjek sebelum melakukan 
+# pengamatan dalam memahami efek dari treatment yang diberikan
+
+# desain eksperimen atau experimental design
+# dalam merancang atau membuat sebuah design setidaknya user harus memperhatikan 
+# kendali (control), pengacakan(randomisation), dan replikasi (replication)
+
+# experimental design (control or kendali)\
+# dalam kontrol akan terdapat banyak terminologi yang akan digunakan
+# terminologi pertama, yaitu experimental unit yang digunakan untuk keseluruhan subject yang dilibatkan dalam suatu experiment
+# dalam experimental unit dibagi menjadi dua kelampok
+# treatment group adalah sebah subject dalam experimental study yang menerima treatment sebelum diamati
+# control group adalah sebuah subject dalam experimental study yang tidak menerima treatment dan juga diamati
+# subject seringkali menerima treatment palsu atau fake treatment atau placebo treatment
+
+# dalam kendali atau control dapat dilakukan dengan menerapkan 
+# blinding adalah subject eksperiment tidak mengetahui apakah dirinya menerima treatment atau placebo
+# double blinding adalah baik peneliti maupun subject eksperiment tidak mengetahui apakah seorang subject menerima treatment atau placebo
+# dan dibutuhkan pihak ketiga untuk mendistribusikan treatment dan placebo kepada sebuah subject exsperiment
