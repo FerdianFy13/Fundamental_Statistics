aacf276366b72b132ded171752d542ad551c8a3a
FerdianFy13 <ferdianfy13@gmail.com>
2022-06-30 20:00:07 +0700

    design experimental study material update

--- Desaign_Experimen/main.py
+++ Desaign_Experimen/main.py
@@ -1,11 +1,11 @@
 # dalam bidang statistika studi statistik dibagi menjadi dua macam, yaitu
 # studi observasi adalah studi dengan melalakukan pengamatan langsung terhadap seubjek tanpa melakukan
-# tindakan apapun yang dapat mempengaruhi hasil pengamatan 
-# studi ekspermin adalah studi dengan menerapkan sebuah treatment tertentu terhadap subjek sebelum melakukan 
+# tindakan apapun yang dapat mempengaruhi hasil pengamatan
+# studi ekspermin adalah studi dengan menerapkan sebuah treatment tertentu terhadap subjek sebelum melakukan
 # pengamatan dalam memahami efek dari treatment yang diberikan
 
 # desain eksperimen atau experimental design
-# dalam merancang atau membuat sebuah design setidaknya user harus memperhatikan 
+# dalam merancang atau membuat sebuah design setidaknya user harus memperhatikan
 # kendali (control), pengacakan(randomisation), dan replikasi (replication)
 
 # experimental design (control or kendali)\
@@ -16,7 +16,19 @@
 # control group adalah sebuah subject dalam experimental study yang tidak menerima treatment dan juga diamati
 # subject seringkali menerima treatment palsu atau fake treatment atau placebo treatment
 
-# dalam kendali atau control dapat dilakukan dengan menerapkan 
+# dalam kendali atau control dapat dilakukan dengan menerapkan
 # blinding adalah subject eksperiment tidak mengetahui apakah dirinya menerima treatment atau placebo
 # double blinding adalah baik peneliti maupun subject eksperiment tidak mengetahui apakah seorang subject menerima treatment atau placebo
 # dan dibutuhkan pihak ketiga untuk mendistribusikan treatment dan placebo kepada sebuah subject exsperiment
+
+# experimental design (randomisation atau pengacakana)
+# adalah proses pengelompokan experimental unit kedalam treatment group dan control group yang dilakukan secara acak atau random
+
+# dalam randomisation atau pengacakan dilakukan dengan menerapkan randomised blocked design
+# experimental unit akan dikelompokkan berdasarkan kesamaan karakteristika tertentu dan setiao subjek akan dipilih secara acak untuk dimasukkan
+# dalam treatment group dan control group
+
+# experimental design replication atau replikasi
+# digunakan untuk meningkatkan validitas dari hasil experiment yang dibutuhkan dalam pengulangan experiment dengan kondisi serupa maupun mirip
+# replikasi melibatkan subject eksperiment atau experiment unitt yang berbeda, sehingga replikasi juga akan memperbesar ukuran dari subject eksperiment atau sample size
+# dalam menunjang validitas dari hasil eksperiment yang dilakukan
