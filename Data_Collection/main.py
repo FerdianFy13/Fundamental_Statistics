385208e379c2e0f94c2f44bd680b1f2616b7cedc
FerdianFy13 <ferdianfy13@gmail.com>
2022-07-01 10:46:41 +0700

    data collection material

---
+++ Data_Collection/main.py
@@ -0,0 +1,32 @@
+# Pengumpulan data atau data collection 
+# dalam bidang statistika terdapat dua cara dalam melakukan pengumpulan data, yaitu
+# census atau sensus
+# adalah pengumpulan data yang dilakukan pada tingkat populasi yang akan menghasilkan informasi yang sifatnya lengkap
+# hanya saja ketika ukuran populasinya sangat besar, sensur akan menjadi pilihan yang mahal dan sulit untuk dilakukan
+# sampling
+# adalah pengumpulan data yang dilakukan pada sub bagian dari populasi, sehingga informasi yang diperoleh sifatnya tidak lengkap
+# pendekatan ini sering digunakan dalam bidang stastika, dimana sample yang baik adalah sample yang dapat merepresentasikan populasinya.
+# dibutuhkan teknik sampling yang tepatt untuk mendapatkan sample yang representatif terhadap populasinya
+
+# sampling error adalah perbedaan atau selisih nilai antara data sample dan data populasi  
+
+# sampling with replacement
+# adalah memungkingkan satu anggota populasi untuk terpilih lebih dari satu kali sebagai anggota sample
+# sampling without replacement
+# yang menjamin satu anggota populasi hanya dapat terpilih satu kali saja sebagai anggota sample
+
+# teknik sampling dalam bidang statistika, yaitu
+# simple random sampling
+# adalah sebuah teknik pengumpulan data yang dilakukan secara acak dimana setiap anggota populasi memiliki peluang yang sama dalam terpilih menjadi anggota sample
+# stratified sampling 
+# adalah sebuah teknik pengumpulan data secara acak yang dilakukan dengana terlebih dahulu dengan membagi anggotat populasi ke dalam beberapa kelompok berdasarkan kesamaan karakteristik tertentu
+# kelompok yang terbentuk dalam stratified sampling disebut dengan strata
+# anggota dari setiap strata tesebut akan dipilih secara acak untuk dijadikan anggota sample
+# sampling yang dilakukan menggunakan teknik ini harus dilakukan secara proporsional dengan proporsinya di tiap strata dalam populasinya
+# cluster sampling
+# adalah sebuah teknik pengumpulan data secara acak yang dilakukan dengan membagai anggota populasi ke dalam beberapa kelompok berdasarkan pengelompokan yang sudah terbentuk (cluster)
+# anggot dari tiap cluster akan dipilih secara acak untuk dijadikan anggota sample
+# sampling yang dilakukan menggunakan teknik ini harus dilakukan secara proporsional dengan proporsinya di tiap cluster dalam populasinya
+# systematics sampling
+# adalah sebuah teknik pengumpulan data berdasarkan interval tertentu dan teknik ini tidak dapat dilakukan bila ditemui adanya pola yang sifatnya konsisten dan sistematis pada data sampling
+# convenience sampling
